1a76fd99af7b7879dfdb41cbb14b53e3934eec3c
HeatherAckerman <hla20115213@gmail.com>
2020-12-01 11:20:27 -0600

    it works --I hope

---
+++ client/build/precache-manifest.edcaa0d2d290e94255008ce8171bfd10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c12a1f390377b35d3d5a",
+    "url": "/static/css/main.98331322.chunk.css"
+  },
+  {
+    "revision": "c12a1f390377b35d3d5a",
+    "url": "/static/js/main.0a416511.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "891005d768795ce460d9",
+    "url": "/static/css/2.d408a9e8.chunk.css"
+  },
+  {
+    "revision": "891005d768795ce460d9",
+    "url": "/static/js/2.fbc74f00.chunk.js"
+  },
+  {
+    "revision": "ec247ac18cae51e7d15826a9cf817502",
+    "url": "/index.html"
+  }
+];
