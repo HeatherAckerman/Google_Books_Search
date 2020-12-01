c1c2d7902e577db3952092e2792d7da7702f62cb
HeatherAckerman <hla20115213@gmail.com>
2020-11-30 20:03:31 -0600

    edited index.html to try to fix heroku issue

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,7 @@ import'bootstrap/dist/js/bootstrap.bundle.min';
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import App from "./App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
